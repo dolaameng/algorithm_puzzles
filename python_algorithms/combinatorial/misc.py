a2f087f011fb1e8018153358fc507d57c5588f0e
dolaameng <dolaameng@gmail.com>
2012-06-10 20:47:34 +0800

    start adding combinatorial/misc

---
+++ python_algorithms/combinatorial/misc.py
@@ -0,0 +1,14 @@
+## Catalan sequences and Catalan numbers
+## generate all sequences of length 2n, composed of 1s and -1s,
+## satisfying the following conditions a) the sum of all terms is 0
+## b) the sum of any prefix is nonnegative.
+## the number of such sequences is called Catalan number.
+## n = 2: ++--, +-+-
+## n = 3: +++---, ++-+--, ++--+-, +-+-+-, +-++--
+def catalan_sequnces(n):
+    pass
+
+## test    
+if __name__ == '__main__':
+    ## test catalan_sequences
+    print catalan_sequences(1)
